90804789d83c23be69ed2a475230789ca95d536d
dosisod <39638017+dosisod@users.noreply.github.com>
2018-08-30 21:46:31 -0700

    adding code.js

---
+++ code.js
@@ -0,0 +1,22 @@
+function save() {
+	document.getElementById("save-form").submit()
+}
+
+function oopen() { //open() is already taken so oopen() is used
+	document.getElementById("file").click()
+	document.getElementById("open-form").onchange=function() {
+		var fl=document.getElementById("file").files[0]
+		var fr=new FileReader()
+		fr.readAsText(fl)
+		fr.onloadend=function() {
+			if (confirm("Opening a new file overrides existing file. Press OK to continue")) {
+				document.getElementById("txt").innerHTML=fr.result
+				document.getElementById("fname").innerHTML=fl.name
+				document.getElementById("open-form").reset()
+			}
+		}
+	}
+}
+function edit() {
+	alert("edit")
+}
