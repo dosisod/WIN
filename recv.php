a19c1b395535591625c92a30cf885cb9b51c5e44
dosisod <39638017+dosisod@users.noreply.github.com>
2019-02-03 21:59:33 -0800

    files can be saved into folders now

--- recv.php
+++ recv.php
@@ -1,13 +1,22 @@
 <?php
 
 $path="/path/to/dir/"; //path to save files to
+$path=(realpath($path)); //dont change this
 
-if (isset($_POST["text"])&&isset($_POST["fname"])) {
-	$file=basename($_POST["fname"]); //string any folder paths
-	file_put_contents($path.$file, $_POST["text"]);
-	echo $_POST["fname"]." was saved successfully";
+if (isset($_POST["text"], $_POST["fname"])) {
+	//converts path of file to real path
+	$path_new=realpath($path."/".$_POST["fname"]);
+
+	//checks if user is using ../ or / in path
+	if (!$path_new||strpos($path_new, $path)!==0) { //file path not safe
+		echo $_POST["fname"]." could not be saved";
+	}
+	else { //file path is safe
+		file_put_contents($_POST["fname"], $_POST["text"]);
+		echo $_POST["fname"]." was saved successfully";
+	}
 }
 else {
-	echo $_POST["fname"]." could not be saved";
+	echo "Invalid params";
 }
 ?>
